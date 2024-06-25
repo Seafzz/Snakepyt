08fcb580503507b7da2fb7ac3f6728d090047b77
Seafzz <163156309+Seafzz@users.noreply.github.com>
2024-06-25 00:18:14 +0000

    added a function that will add to the player interaction if they have played this game before

--- run.py
+++ run.py
@@ -12,3 +12,20 @@ def player_name(stdscr):
     player = stdscr.getstr().decode(encoding="Utf-8")
     curses.noecho() #Disable text input
     return player
+
+ def ask_played_before(stdscr):
+        """
+        Function to ask if the player has played before.
+        """
+        stdscr.clear() #Clear the screen
+        stdscr.addstr(0,0, "Have you played before? (y/n)")
+        stdscr.refresh()
+        while True:
+            played before == stdscr.getstr().decode(encoding="utf-8").lower()
+            if played_before in {'yes', 'y', 'no', 'n'}:
+                return played_before
+            else:
+                stdscr.addstr(1,0, "Please enter 'yes' or 'no'")
+                stdscr.refresh()
+                
+        
