7e8ac400d3f84ca2f767a2856fba902951192e4a
Seafzz <163156309+Seafzz@users.noreply.github.com>
2024-06-25 00:19:06 +0000

    Added a function to show the instructions for the game

--- run.py
+++ run.py
@@ -27,5 +27,19 @@ def player_name(stdscr):
             else:
                 stdscr.addstr(1,0, "Please enter 'yes' or 'no'")
                 stdscr.refresh()
-                
+def print_instructions(stdscr):
+    """
+    Function to print the instructions of the game.
+    """
+    stdscr.clear()
+    stdscr.addstr(0,0, "Instructions:")
+    stdscr.addstr(1,0, "1. The player has to guess the number between 1 and 100.")
+    stdscr.addstr(2,0, "2. The player has 10 attempts to guess the number.")
+    stdscr.addstr(3,0, "3. After each guess, the player will be told if the number is higher or lower.")
+    stdscr.addstr(4,0, "4. If the player guesses the number, the game will end.")
+    stdscr.addstr(5,0, "5. If the player runs out of attempts, the game will end.")
+    stdscr.addstr(6,0, "6. Good luck!")
+    stdscr.addstr(7,0, "Press any key to continue...")
+    stdscr.refresh()
+    stdscr.getch()
         
