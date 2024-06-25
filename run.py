9cdde9a10b7c60872f54aeee5ade3f6d0832fc30
Seafzz <163156309+Seafzz@users.noreply.github.com>
2024-06-25 00:13:18 +0000

    added a function to get the player name from the terminal

--- run.py
+++ run.py
@@ -1,2 +1,14 @@
 import random
 import curses
+
+def player_name(stdscr):
+    """
+    Function to get the player's name.
+    """
+    stdscr.clear()
+    curses.echo() #Enable text input
+    stdscr.addstr(0,0 "What's your name?")
+    stdscr.refresh()
+    player = stdscr.getstr().decode(encoding="Utf-8")
+    curses.noecho() #Disable text input
+    return player
